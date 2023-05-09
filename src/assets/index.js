66a07e67a00d6eb0fcb332b5833be67ef1e7e79b
Shibam Saha <shibamsaha00@gmail.com>
2023-05-09 06:57:32 +0530

    added new assets

---
+++ src/assets/index.js
@@ -0,0 +1,26 @@
+
+import certificateTemplate from "./Certificate-Template.pdf";
+import bgGradient from "./images/Background-Gradient.webp";
+import coding from "./images/Coding.webp";
+import error from "./images/Error.webp";
+import hero from "./images/Hero.webp";
+import login from "./images/Login.webp";
+import logo from "./images/Logo.webp";
+import reset from "./images/Reset.webp";
+import scoreCardImg from "./images/Score-Card.webp";
+import signup from "./images/Signup.webp";
+
+
+export {
+    certificateTemplate,
+    bgGradient,
+    coding,
+    error,
+    hero,
+    login,
+    logo,
+    reset,
+    scoreCardImg,
+    signup
+};
+
