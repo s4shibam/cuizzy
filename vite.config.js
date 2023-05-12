4bf9b449d5a69e0c948810a0a5e863b4ca5b97a5
Shibam Saha <shibamsaha00@gmail.com>
2023-05-12 07:17:06 +0530

    added build minification

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    minify: false
-  },
   server: {
     host: true
   }
