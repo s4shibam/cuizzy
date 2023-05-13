90b3fe65ee93343080867267eca25d2047fd95ab
Shibam Saha <shibamsaha00@gmail.com>
2023-05-14 02:26:35 +0530

    modified jsx

--- src/pages/Learn.jsx
+++ src/pages/Learn.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { DesignComponent, Thumbnail } from '../components';
-import useData from '../hooks';
+import { useData } from '../hooks';
 
 function Learn() {
   const { loading, error, data } = useData('videos');
