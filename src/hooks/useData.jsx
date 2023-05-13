d67b0540f0b768ad8b9a4246cc85d98aa161fcfb
Shibam Saha <shibamsaha00@gmail.com>
2023-05-14 02:22:07 +0530

    modified comment

--- src/hooks/useData.jsx
+++ src/hooks/useData.jsx
@@ -7,7 +7,7 @@ export default function useData(address) {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    // Fetch quiz topic list from database
+    // Fetch video list from database
     async function fetchTopics() {
       const db = getDatabase();
       const dataRef = ref(db, address);
