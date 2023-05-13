43708fb3134d7a70e6031473d3c861a83886e9c1
Shibam Saha <shibamsaha00@gmail.com>
2023-05-14 01:17:10 +0530

    added new placeholder image

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,26 +1,25 @@
-
-import certificateTemplate from "./Certificate-Template.pdf";
-import bgGradient from "./images/Background-Gradient.webp";
-import coding from "./images/Coding.webp";
-import error from "./images/Error.webp";
-import hero from "./images/Hero.webp";
-import login from "./images/Login.webp";
-import logo from "./images/Logo.webp";
-import reset from "./images/Reset.webp";
-import scoreCardImg from "./images/Score-Card.webp";
-import signup from "./images/Signup.webp";
-
+import certificateTemplate from './Certificate-Template.pdf';
+import bgGradient from './images/Background-Gradient.webp';
+import coding from './images/Coding.webp';
+import error from './images/Error.webp';
+import hero from './images/Hero.webp';
+import login from './images/Login.webp';
+import logo from './images/Logo.webp';
+import placeholder from './images/Placeholder.webp';
+import reset from './images/Reset.webp';
+import scoreCardImg from './images/Score-Card.webp';
+import signup from './images/Signup.webp';
 
 export {
-    certificateTemplate,
-    bgGradient,
-    coding,
-    error,
-    hero,
-    login,
-    logo,
-    reset,
-    scoreCardImg,
-    signup
+  certificateTemplate,
+  bgGradient,
+  coding,
+  error,
+  hero,
+  login,
+  logo,
+  placeholder,
+  reset,
+  scoreCardImg,
+  signup
 };
-
