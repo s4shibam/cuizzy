c2d4f75e2040ac4740d69027983b25303982fdee
Shibam Saha <shibamsaha00@gmail.com>
2023-05-14 03:14:09 +0530

    modified jsx

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,52 +1,33 @@
-import _ from 'lodash';
 import { useState } from 'react';
-import { Link, useLocation, useParams } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { DesignComponent, DetailedReport, ResultSummary } from '../components';
-import { useAnswers } from '../hooks';
 
 function Result() {
-  const { id } = useParams();
   const location = useLocation();
-  const { qna } = location.state;
-  const { loading, error, answers } = useAnswers(id);
+  const { qnaSet, markSheetObject } = location.state;
   const [showAnswers, setShowAnswers] = useState(false);
 
-  function calculateScore() {
-    let count = 0;
-
-    answers.forEach((question, index1) => {
-      const correctIndexes = [];
-      const checkedIndexes = [];
-
-      question.options.forEach((option, index2) => {
-        if (option.correct) correctIndexes.push(index2);
-        if (qna[index1].options[index2].checked) {
-          checkedIndexes.push(index2);
-          option.checked = true;
-        }
-      });
-
-      if (_.isEqual(correctIndexes, checkedIndexes)) {
-        count += 1;
-      }
-    });
-
-    return count;
-  }
-
-  const correctAnswers = calculateScore();
+  const {
+    noq,
+    topicId,
+    correctAnswersCount,
+    incorrectAnswersCount,
+    unattemptedCount,
+    obtainedPoints,
+    obtainedPercentage
+  } = markSheetObject;
 
   return (
     <div className='result mx-auto mt-44 mb-20 w-[85%] animate-reveal 2xl:mt-32'>
-      {loading && <p className='text-center'>Loading ...</p>}
-      {error && <p className='text-center'>There was an error!</p>}
-
-      {answers && answers.length > 0 && (
+      {qnaSet?.length > 0 && (
         <>
           <ResultSummary
-            correctAnswers={correctAnswers}
-            noq={answers.length}
-            userAnswers={answers}
+            noq={noq}
+            correctAnswersCount={correctAnswersCount}
+            incorrectAnswersCount={incorrectAnswersCount}
+            unattemptedCount={unattemptedCount}
+            obtainedPoints={obtainedPoints}
+            obtainedPercentage={obtainedPercentage}
           />
 
           <div className='mt-16 flex w-full items-center justify-center gap-6'>
@@ -61,7 +42,7 @@ function Result() {
               </button>
             )}
 
-            <Link to={`/quiz/${id}`}>
+            <Link to={`/quiz/${topicId}`}>
               <button
                 type='button'
                 className='border-button border border-darkViolet px-4 py-2 uppercase'
@@ -72,7 +53,7 @@ function Result() {
             </Link>
           </div>
 
-          {showAnswers && <DetailedReport userAnswers={answers} />}
+          {showAnswers && <DetailedReport qnaSet={qnaSet} />}
         </>
       )}
 
