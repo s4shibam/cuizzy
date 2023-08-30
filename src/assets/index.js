c755c69a37c5205141f4079e2a46e54b0f35199c
s4shibam <shibamsaha2002@gmail.com>
2023-08-30 15:38:50 +0530

    feat: added new logo

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,6 @@
-export { default as certificateTemplate } from './Certificate-Template.pdf';
+export { default as logo } from '../../public/Logo.svg';
+export { default as certificateTemplate } from './Quizzy_Certificate.pdf';
 export { default as coding } from './images/Coding.webp';
 export { default as error } from './images/Error.svg';
 export { default as hero } from './images/Hero.webp';
-export { default as logo } from './images/Logo.svg';
 export { default as placeholder } from './images/Placeholder.webp';
