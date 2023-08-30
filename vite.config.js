fa892eb86c2d01076a0f9e62c5173b4e042046b8
s4shibam <shibamsaha2002@gmail.com>
2023-08-31 04:27:19 +0530

    fix: increased chunkSizeWarningLimit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslintPlugin()],
+  plugins: [react()],
   server: {
     host: true
   },
