a3ec0df278d825ec0f9bc728961b1a3adbcf6982
s4shibam <shibamsaha2002@gmail.com>
2023-08-31 06:04:55 +0530

    feat: added footer

--- src/pages/Learn.jsx
+++ src/pages/Learn.jsx
@@ -1,30 +1,44 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import { Thumbnail } from '../components';
+import { Footer, Thumbnail } from '../components';
 import { useData } from '../hooks';
 
 function Learn() {
   const { loading, error, data } = useData('videos');
+  const [shuffledData, setShuffledData] = useState([]);
 
+  useEffect(() => {
+    if (data.length > 0) {
+      const shuffledArray = [...data];
+      for (let i = shuffledArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+      }
+      setShuffledData(shuffledArray);
+    }
+  }, [data]);
   return (
-    <div className="mx-auto flex w-[85%] animate-reveal flex-col justify-center">
-      <h1 className="page-heading">Learn from us</h1>
+    <>
+      <div className="mx-auto mb-32 flex min-h-screen w-[90%] animate-reveal flex-col items-center">
+        <h1 className="page-heading">Learn from us</h1>
 
-      <div className="mx-auto grid w-full grid-cols-quizzes justify-items-center gap-5">
-        {data.map((video, index) => (
-          <Link key={index} to={`/video/${video.link}`}>
-            <Thumbnail id={video.link} title={video.title} type="video" />
-          </Link>
-        ))}
-      </div>
+        <div className="mx-auto grid w-full grid-cols-quizzes justify-items-center gap-5">
+          {shuffledData.map((video, index) => (
+            <Link key={index} to={`/video/${video.link}`}>
+              <Thumbnail id={video.link} title={video.title} type="video" />
+            </Link>
+          ))}
+        </div>
 
-      <div className="flex items-center justify-center text-center text-xl">
-        {!loading && data.length === 0 && <>No data found! </>}
-        {error && <>There was an error! </>}
-        {loading && <>Loading ...</>}
+        <div className="flex items-center justify-center text-center text-xl">
+          {!loading && shuffledData.length === 0 && <>No data found! </>}
+          {error && <>There was an error! </>}
+          {loading && <>Loading ...</>}
+        </div>
       </div>
-      <br />
-    </div>
+      <Footer />
+    </>
   );
 }
 
--- src/pages/Quizzes.jsx
+++ src/pages/Quizzes.jsx
@@ -1,38 +1,39 @@
 import { Link } from 'react-router-dom';
 
-import { Thumbnail } from '../components';
+import { Footer, Thumbnail } from '../components';
 import { useData } from '../hooks';
 
 function Quizzes() {
   const { loading, error, data } = useData('topics');
 
   return (
-    <div className="mx-auto flex w-[85%] animate-reveal flex-col justify-center">
-      <h1 className="page-heading">Attempt Quizzes</h1>
-      {data.length > 0 && (
-        <div className="mx-auto grid h-full w-full grid-cols-quizzes justify-items-center gap-5">
-          {data.map((topic, index) =>
-            topic.noq > 0 ? (
-              <Link key={topic.topicID} to={`/quiz/${topic.topicID}`}>
-                <Thumbnail id={topic.topicID} noq={topic.noq} title={topic.title} type="quiz" />
-              </Link>
-            ) : (
-              <div key={index} className="w-full">
-                <Thumbnail id={topic.topicID} noq={topic.noq} title={topic.title} type="quiz" />
-              </div>
-            )
-          )}
+    <>
+      <div className="mx-auto mb-32 flex min-h-screen w-[90%] animate-reveal flex-col items-center">
+        <h1 className="page-heading">Attempt Quizzes</h1>
+        {data.length > 0 && (
+          <div className="mx-auto grid h-full w-full grid-cols-quizzes justify-items-center gap-7">
+            {data.map((topic, index) =>
+              topic.noq > 0 ? (
+                <Link key={topic.topicID} to={`/quiz/${topic.topicID}`}>
+                  <Thumbnail id={topic.topicID} noq={topic.noq} title={topic.title} type="quiz" />
+                </Link>
+              ) : (
+                <div key={index} className="w-full">
+                  <Thumbnail id={topic.topicID} noq={topic.noq} title={topic.title} type="quiz" />
+                </div>
+              )
+            )}
+          </div>
+        )}
+
+        <div className="flex items-center justify-center text-center text-xl">
+          {!loading && data.length === 0 && <>No data found!</>}
+          {error && <>There was an error!</>}
+          {loading && <>Loading ...</>}
         </div>
-      )}
-
-      <div className="flex items-center justify-center text-center text-xl">
-        {!loading && data.length === 0 && <>No data found!</>}
-        {error && <>There was an error!</>}
-        {loading && <>Loading ...</>}
       </div>
-
-      <br />
-    </div>
+      <Footer />
+    </>
   );
 }
 
