bf6b106eb139566912834841bd1551a88cd552a6
s4shibam <shibamsaha2002@gmail.com>
2023-12-08 23:07:12 +0530

    feat: contact form submission using email js

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,85 +1,12 @@
-import { useState } from 'react';
-
-import { Form, TextInput } from './';
-
-import showAlert from './AlertList';
+import ContactUsForm from './forms/ContactUsForm';
 
 function ContactUs() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [subject, setSubject] = useState('');
-  const [message, setMessage] = useState('');
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    showAlert('success', 'mail-sent');
-    setName('');
-    setEmail('');
-    setSubject('');
-    setMessage('');
-  }
-
   return (
     <div className="mb-28 mt-10 w-[85%] max-w-2xl">
       <p className="mb-14 text-center text-4xl font-bold uppercase tracking-wider lg:text-5xl">
         Contact Us
       </p>
-      <Form className="card mx-auto" onSubmit={(e) => handleSubmit(e)}>
-        <div className="flex flex-col gap-4">
-          <TextInput
-            required
-            icon="account_circle"
-            placeholder="Enter Name"
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <TextInput
-            required
-            icon="mark_as_unread"
-            placeholder="Enter Email ID"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <TextInput
-            required
-            icon="subject"
-            placeholder="Enter Subject"
-            type="text"
-            value={subject}
-            onChange={(e) => setSubject(e.target.value)}
-          />
-          <div className="flex w-full items-center rounded-md border border-black/30 bg-white p-2 outline-none dark:border-white/30 dark:bg-black/50">
-            <textarea
-              required
-              className="ml-1 h-[100px] w-full resize-none rounded-lg border-none bg-transparent font-medium tracking-wide text-black outline-none dark:text-white lg:text-xl"
-              placeholder="Your Message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            />
-            <span className="material-symbols-outlined mx-1 mb-auto mt-1 flex cursor-pointer items-center justify-center text-black dark:text-white md:text-3xl">
-              comment
-            </span>
-          </div>
-          <div className="flex gap-5">
-            <button
-              className="fill-button mt-1 w-full bg-red-500 hover:bg-red-400"
-              onClick={() => {
-                setName('');
-                setEmail('');
-                setSubject('');
-                setMessage('');
-              }}
-            >
-              Cancel
-            </button>
-            <button className="fill-button mt-1 w-full" type="submit">
-              Send
-            </button>
-          </div>
-        </div>
-      </Form>
+      <ContactUsForm />
     </div>
   );
 }
---
+++ src/components/forms/ContactUsForm.jsx
@@ -0,0 +1,105 @@
+import { send as sendEmail } from '@emailjs/browser';
+import { useState } from 'react';
+
+import showAlert from '../AlertList';
+import Form from '../elements/Form';
+import TextInput from '../elements/TextInput';
+
+function ContactUsForm() {
+  const [senderDetails, setSenderDetails] = useState({});
+  const [loading, setLoading] = useState(0);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+    try {
+      await sendEmail(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        {
+          subject: senderDetails?.subject,
+          userName: senderDetails?.name,
+          message: senderDetails?.message,
+          userEmail: senderDetails?.email
+        },
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+      );
+      setLoading(false);
+      showAlert('success', 'mail-sent');
+    } catch (error) {
+      console.error(error);
+      showAlert('error', error?.text || 'unknown-error');
+      setLoading(false);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setSenderDetails((prevState) => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  return (
+    <Form className="card mx-auto" onSubmit={handleSubmit}>
+      <div className="flex flex-col gap-4">
+        <TextInput
+          required
+          icon="account_circle"
+          placeholder="Enter Name"
+          type="text"
+          value={senderDetails?.name}
+          onChange={handleInputChange}
+        />
+        <TextInput
+          required
+          icon="mark_as_unread"
+          placeholder="Enter Email ID"
+          type="email"
+          value={senderDetails?.email}
+          onChange={handleInputChange}
+        />
+        <TextInput
+          required
+          icon="subject"
+          placeholder="Enter Subject"
+          type="text"
+          value={senderDetails?.subject}
+          onChange={handleInputChange}
+        />
+        <div className="flex w-full items-center rounded-md border border-black/30 bg-white p-2 outline-none dark:border-white/30 dark:bg-black/50">
+          <textarea
+            required
+            className="ml-1 h-[100px] w-full resize-none rounded-lg border-none bg-transparent font-medium tracking-wide text-black outline-none dark:text-white lg:text-xl"
+            placeholder="Your Message"
+            value={senderDetails?.message}
+            onChange={handleInputChange}
+          />
+          <span className="material-symbols-outlined mx-1 mb-auto mt-1 flex cursor-pointer items-center justify-center text-black dark:text-white md:text-3xl">
+            comment
+          </span>
+        </div>
+        <div className="flex gap-5">
+          <button
+            className="fill-button mt-1 w-full bg-red-500 hover:bg-red-400"
+            disabled={loading}
+            onClick={() => setSenderDetails({})}
+          >
+            Cancel
+          </button>
+          <button
+            className="fill-button mt-1 w-full disabled:opacity-70"
+            disabled={loading}
+            type="submit"
+          >
+            {loading ? 'Sending...' : 'Send'}
+          </button>
+        </div>
+      </div>
+    </Form>
+  );
+}
+
+export default ContactUsForm;
