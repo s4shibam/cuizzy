f08fcfb094f842e80c22eecd03b41a754244eca4
s4shibam <shibamsaha2002@gmail.com>
2023-12-08 23:06:07 +0530

    update: animate once

--- src/components/CountUpAnimation.jsx
+++ src/components/CountUpAnimation.jsx
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import CountUp from 'react-countup';
 import VisibilitySensor from 'react-visibility-sensor';
 
 function CountUpAnimation({ end }) {
+  const [hasAnimated, setHasAnimated] = useState(false);
+
+  const onVisibilityChange = (isVisible) => {
+    if (isVisible && !hasAnimated) {
+      setHasAnimated(true);
+    }
+  };
+
   return (
     <>
       <CountUp delay={0.1} end={end} redraw={true}>
         {({ countUpRef, start }) => (
-          <VisibilitySensor delayedCall onChange={start}>
+          <VisibilitySensor
+            delayedCall
+            onChange={(isVisible) => onVisibilityChange(isVisible) && start()}
+          >
             <span ref={countUpRef} />
           </VisibilitySensor>
         )}
