850b29b7cab04a13c4f9389d4e2869e2ea24a460
s4shibam <shibamsaha2002@gmail.com>
2023-12-08 21:43:12 +0530

    refactor: remove redundancy in ui components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,60 +15,42 @@ function Footer() {
           </Link>
         </div>
         <div className="grid grid-cols-2 gap-8 sm:grid-cols-3">
-          <div>
-            <h2 className="mb-4 text-sm font-semibold uppercase">Resources</h2>
-            <ul>
-              <li className="mb-4">
-                <Link className="hover:underline" to="/quizzes">
-                  Quizzes
-                </Link>
-              </li>
-              <li>
-                <Link className="hover:underline" to="/learn">
-                  Learn
-                </Link>
-              </li>
-            </ul>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-sm font-semibold uppercase">Resources</h2>
+            <Link className="hover:underline" to="/quizzes">
+              Quizzes
+            </Link>
+            <Link className="hover:underline" to="/learn">
+              Learn
+            </Link>
           </div>
-          <div>
-            <h2 className="mb-4 text-sm font-semibold uppercase">Follow us</h2>
-            <ul>
-              <li className="mb-4">
-                <a
-                  className="hover:underline "
-                  href="https://github.com/s4shibam"
-                  rel="noreferrer"
-                  target="_blank"
-                >
-                  Github
-                </a>
-              </li>
-              <li>
-                <a
-                  className="hover:underline"
-                  href="https://www.linkedin.com/in/s4shibam"
-                  rel="noreferrer"
-                  target="_blank"
-                >
-                  LinkedIn
-                </a>
-              </li>
-            </ul>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-sm font-semibold uppercase">Follow us</h2>
+            <a
+              className="hover:underline "
+              href="https://github.com/s4shibam"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Github
+            </a>
+            <a
+              className="hover:underline"
+              href="https://www.linkedin.com/in/s4shibam"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              LinkedIn
+            </a>
           </div>
-          <div>
-            <h2 className="mb-4 text-sm font-semibold uppercase">Legal</h2>
-            <ul>
-              <li className="mb-4">
-                <Link className="hover:underline" href="/">
-                  Privacy Policy
-                </Link>
-              </li>
-              <li>
-                <Link className="hover:underline" href="/">
-                  T&amp;C
-                </Link>
-              </li>
-            </ul>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-sm font-semibold uppercase">Legal</h2>
+            <Link className="hover:underline" href="/">
+              Privacy Policy
+            </Link>
+            <Link className="hover:underline" href="/">
+              T&amp;C
+            </Link>
           </div>
         </div>
       </div>
@@ -77,14 +59,14 @@ function Footer() {
         <div className="text-center md:text-left">
           <span className="mb-1 block ">
             Developed with 💚 by &nbsp;
-            <Link
+            <a
               className="cursor-pointer hover:underline"
               href="https://www.shibamsaha.dev"
-              rel="noreferrer"
+              rel="noopener noreferrer"
               target="_blank"
             >
               Shibam Saha.
-            </Link>
+            </a>
           </span>
           <span>
             © 2023{' '}
@@ -95,7 +77,7 @@ function Footer() {
           </span>
         </div>
         <div className="mx-auto mt-4 flex w-fit space-x-6 sm:justify-center md:mx-0 md:mt-0">
-          <a href="https://www.facebook.com/s4shibam" rel="noreferrer" target="_blank">
+          <a href="https://www.facebook.com/s4shibam" rel="noopener noreferrer" target="_blank">
             <svg aria-hidden="true" className="h-5 w-5 dark:fill-white" viewBox="0 0 24 24">
               <path
                 clipRule="evenodd"
@@ -105,7 +87,7 @@ function Footer() {
             </svg>
             <span className="sr-only">Facebook</span>
           </a>
-          <a href="https://www.instagram.com/s4shibam" rel="noreferrer" target="_blank">
+          <a href="https://www.instagram.com/s4shibam" rel="noopener noreferrer" target="_blank">
             <svg aria-hidden="true" className="h-5 w-5 dark:fill-white" viewBox="0 0 24 24">
               <path
                 clipRule="evenodd"
@@ -115,13 +97,13 @@ function Footer() {
             </svg>
             <span className="sr-only">Instagram</span>
           </a>
-          <a href="https://twitter.com/s4shibam" rel="noreferrer" target="_blank">
+          <a href="https://twitter.com/s4shibam" rel="noopener noreferrer" target="_blank">
             <svg aria-hidden="true" className="h-5 w-5 dark:fill-white" viewBox="0 0 24 24">
               <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
             </svg>
             <span className="sr-only">Twitter</span>
           </a>
-          <a href="https://github.com/s4shibam" rel="noreferrer" target="_blank">
+          <a href="https://github.com/s4shibam" rel="noopener noreferrer" target="_blank">
             <svg aria-hidden="true" className="h-5 w-5 dark:fill-white" viewBox="0 0 24 24">
               <path
                 clipRule="evenodd"
