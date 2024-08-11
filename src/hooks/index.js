08aa684269c637525dfb35c12c859fa3fe1b29af
s4shibam <shibam@prodgain.com>
2024-08-11 22:06:07 +0530

    fix: fix import error

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,7 +2,7 @@ export { default as useAlert } from './useAlert';
 export { default as useCertificate } from './useCertificate';
 export { default as useData } from './useData';
 export { default as useFileSaver } from './useFileSaver';
-export { default as useGAEventTracker } from './useGaEventTracker';
+export { default as useGAEventTracker } from './useGAEventTracker';
 export { default as usePopularQuizzes } from './usePopularQuizzes';
 export { default as useQuiz } from './useQuiz';
 export { default as useSubmissions } from './useSubmissions';
